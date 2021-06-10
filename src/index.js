44fab8c74960c6324daa363c2fe4a6b735f23aed
Marcin Wołkowicz <mwolkowicz17@gmail.com>
2021-06-10 08:51:29 +0200

    first

---
+++ src/index.js
@@ -0,0 +1,75 @@
+import * as THREE from 'three';
+import * as OrbitControls from 'three-orbitcontrols';
+import { Scene } from 'three';
+import {cube} from './cube.js';
+import {line} from './line.js';
+import {light} from './hemi_light.js';
+import {light2} from  './directional_light.js';
+import {cylinder} from './roof.js';
+
+
+// input segment
+const btn = document.getElementById("action");
+
+btn.addEventListener("click", function () {
+    //Do something here
+    let boxwidthinput = document.getElementById("myInput").value;
+    let boxwidthinputnumber = parseFloat(boxwidthinput);
+    if (boxwidthinputnumber != 0) {
+        cube.scale.x = boxwidthinputnumber;
+    }
+
+    //cube.position.x=2;
+});
+// position segment
+
+const btnposition = document.getElementById("actionPosition");
+
+btnposition.addEventListener("click", function(){
+    let boxpositionxinput = document.getElementById("positionInput").value;
+    let boxpositionxinputnumber = parseFloat(boxpositionxinput);
+    if (boxpositionxinputnumber != 0) {
+        cube.position.x = boxpositionxinputnumber;
+    }
+})
+//cube.position.x=2; do zrobienia
+
+
+
+
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+
+const renderer = new THREE.WebGLRenderer({ antialias: true });
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
+
+//cube.add(line);
+cube.add(cylinder);
+scene.add(cube);
+scene.add(light);
+scene.add(light2);
+scene.add(light2.target);
+
+camera.position.z = 5;
+
+const controls = new OrbitControls( camera, renderer.domElement );
+
+//controls.update() must be called after any manual changes to the camera's transform
+//camera.position.set( 0, 20, 100 );
+controls.update();
+controls.autoRotate=true;
+
+const animate = function () {
+    requestAnimationFrame(animate);
+
+    //cube.rotation.x += 0.02;
+    //cube.rotation.y += 0.01;
+
+    renderer.render(scene, camera);
+    
+    controls.update();
+
+};
+
+animate();
---
+++ src/roof.js
@@ -0,0 +1,11 @@
+import * as THREE from 'three';
+
+const geometry3 = new THREE.CylinderGeometry( 1, 1, 5, 3, 1, false, 0 );
+const material3 = new THREE.MeshPhongMaterial( {color: 0xffff00} );
+export const cylinder = new THREE.Mesh( geometry3, material3 );
+cylinder.rotation.z = 1.57;
+cylinder.rotation.x = 4.71;
+cylinder.scale.x = 0.7;
+cylinder.scale.y = 0.44;
+cylinder.scale.z = 0.5;
+cylinder.position.y=0.7;
