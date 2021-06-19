5733907092c3c9cf818ea6ab096eb5ac92339488
Marcin Wołkowicz <mwolkowicz17@gmail.com>
2021-06-19 09:50:44 +0200

    bootom tree work

--- src/tree_bottom1.js
+++ src/tree_bottom1.js
@@ -1,12 +1,13 @@
 import * as THREE from 'three';
 
-const radiusTop =  0.2;
-const radiusBottom =  0.2;
-const height =  2.3;
+const radiusTop =  0.1;
+const radiusBottom =  0.1;
+const height =  1;
 const radialSegments = 27; 
 const geometry = new THREE.CylinderGeometry(radiusTop, radiusBottom, height, radialSegments);
-const material = new THREE.MeshPhongMaterial({ color: 0x38761d });
+const material = new THREE.MeshPhongMaterial({ color: 0x713b1c });
 
 export const treebottom1 = new THREE.Mesh(geometry, material);
 
 treebottom1.position.x=-2;
+treebottom1.position.z=2;
