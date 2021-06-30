cd3f4c04db963859e2871957a890a4fa302408f3
Marcin Wołkowicz <mwolkowicz17@gmail.com>
2021-06-30 16:00:00 +0200

    second part of front wall

--- src/wall_front.js
+++ src/wall_front.js
@@ -1,7 +1,10 @@
 import * as THREE from 'three';
 
-const width = 5;
+//const width = 5;
+const width = 2.4;
+//const height = 0.4; 
 const height = 0.4; 
+//const depth = 0.01;
 const depth = 0.01;
 const geometry = new THREE.BoxGeometry(width, height,depth);
 const material = new THREE.MeshPhongMaterial({ color: 0xb5651d });
@@ -9,6 +12,15 @@ const material = new THREE.MeshPhongMaterial({ color: 0xb5651d });
 export const wallfront = new THREE.Mesh(geometry, material);
 
 wallfront.position.z = 4.5;
-wallfront.position.x= -0.4;
+//wallfront.position.x= -0.4;
+wallfront.position.x= -1.7;
 wallfront.position.y= -0.26;
 wallfront.castShadow = true;
+
+export const wallfrontB = new THREE.Mesh(geometry, material);
+
+wallfrontB.position.z = 4.5;
+//wallfront.position.x= -0.4;
+wallfrontB.position.x= -1.7;
+wallfrontB.position.y= -0.26;
+wallfrontB.castShadow = true;
